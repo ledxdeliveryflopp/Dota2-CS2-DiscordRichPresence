3d308911105ada19554873936958db3c30910428
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-02-23 02:34:59 +0700

    Добавил сохранения времени игры и статус "в меню"

--- internal/discord/handler/handler.go
+++ internal/discord/handler/handler.go
@@ -2,21 +2,20 @@ package handler
 
 import (
 	apiTypes "discord_dota2/internal/api/types"
+	"discord_dota2/internal/discord"
 	"discord_dota2/internal/discord/types"
 	"github.com/hugolgst/rich-go/client"
-	"time"
 )
 
 func SetDiscordPresence(success chan bool, error chan error, player *apiTypes.Player, hero *apiTypes.Hero) {
 	var presence types.Presence
 	presence.SetPresenceInfo(player, hero)
-	now := time.Now()
 	err := client.SetActivity("1342725398261403668", client.Activity{
 		State:      presence.State,
 		Details:    presence.Details,
 		LargeImage: presence.HeroCode,
 		LargeText:  presence.HeroReadableName,
-		SmallImage: "main",
+		SmallImage: presence.SmallImage,
 		SmallText:  "Dota 2",
 		//Party: &client.Party{
 		//	ID:         "-1",
@@ -24,7 +23,7 @@ func SetDiscordPresence(success chan bool, error chan error, player *apiTypes.Pl
 		//	MaxPlayers: 24,
 		//},
 		Timestamps: &client.Timestamps{
-			Start: &now,
+			Start: &discord.GameTime,
 		},
 	})
 	if err != nil {
