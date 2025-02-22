74d1c0b7a013de6c9b3f81ff0913cd730988fd23
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-02-22 23:59:55 +0700

    Добавлена структура для установки текста в Discord

---
+++ internal/discord/types/presence.go
@@ -0,0 +1,28 @@
+package types
+
+import (
+	"discord_dota2/internal/api/types"
+	"discord_dota2/internal/discord"
+	"fmt"
+	"log"
+)
+
+type Presence struct {
+	State            string // KDA мб голда
+	Details          string // Персонаж
+	HeroCode         string // Код персонажа
+	HeroReadableName string // Имя персонажа
+}
+
+func (p *Presence) SetPresenceInfo(player *types.Player, hero *types.Hero) {
+	p.HeroCode = hero.Name
+	p.HeroReadableName = discord.Heroes[hero.Name]
+	p.State = fmt.Sprintf("KDA: %d/%d/%d,  lvl: %d, gold: %d",
+		player.Kills, player.Deaths, player.Assists, hero.Level, player.Gold)
+	if len(p.HeroReadableName) == 0 {
+		log.Printf("Can't find hero in map: %s", hero.Name)
+		p.Details = fmt.Sprintf("Персонаж: Неизвестно - %d%%HP", hero.HealthPercent)
+	} else {
+		p.Details = fmt.Sprintf("Персонаж: %s - %d%%HP", p.HeroReadableName, hero.HealthPercent)
+	}
+}
