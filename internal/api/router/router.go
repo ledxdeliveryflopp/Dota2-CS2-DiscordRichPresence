4c37eb085be29826dbcb61a9d0618e0da973c21c
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-02-25 22:02:28 +0700

    Добавил роутер для CS:GO

--- internal/api/router/router.go
+++ internal/api/router/router.go
@@ -1,10 +1,12 @@
 package router
 
 import (
-	"discord_dota2/internal/api/handler"
+	"discord_dota2_cs2/internal/api/handler"
 	"github.com/gorilla/mux"
 )
 
 func SetIntegrationRouter(router *mux.Router) {
-	router.HandleFunc("/", handler.HandleGameStateResponse).Methods("POST")
+	gsiRouter := router.PathPrefix("/handler").Subrouter()
+	gsiRouter.HandleFunc("/dota/", handler.HandleDotaGameStateResponse).Methods("POST")
+	gsiRouter.HandleFunc("/cs/", handler.HandleCsGoGameStateResponse).Methods("POST")
 }
