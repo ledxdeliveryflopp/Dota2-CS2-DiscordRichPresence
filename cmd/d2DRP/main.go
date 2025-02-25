885dfb102943046f3be1fd86ff1d9db9316a1976
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-02-25 22:00:38 +0700

    Обновил точку входа
    1) добавил запуск в горутине, функции для проверки процесса игры
    2) Добавил функцию инициализаци Logrus

--- cmd/d2DRP/main.go
+++ cmd/d2DRP/main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
-	"discord_dota2/internal/api/router"
-	discordInit "discord_dota2/internal/discord/init"
+	"discord_dota2_cs2/internal/api/router"
+	discordInit "discord_dota2_cs2/internal/discord/init"
+	"fmt"
 	"github.com/gorilla/mux"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"net/http"
+	"os"
 )
 
-func main() {
-	err := discordInit.InitDiscordClient()
+func initLogrus() {
+	file, err := os.OpenFile("logrus.log", os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
-		panic(err)
+		log.Info("error while create log file, using base std")
+		log.Error("error from initLogrus:", err)
+		return
 	}
+	log.SetOutput(file)
+	log.Info("Logrus initer")
+}
+
+func main() {
+	initLogrus()
+	discordInit.InitDiscordClient()
+	go discordInit.CheckGameIsRunning()
 	mainRouter := mux.NewRouter()
 	router.SetIntegrationRouter(mainRouter)
-	log.Println("application started")
-	err = http.ListenAndServe(":52424", mainRouter)
+	fmt.Println("start api")
+	err := http.ListenAndServe(":52424", mainRouter)
 	if err != nil {
-		panic(err)
+		log.Panicln("error while starting api:", err)
 	}
 }
